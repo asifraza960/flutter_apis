dece3a6c397143f3fdec0e5fc798a2a849f52e89
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2025-05-13 16:57:58 +0500

    update

--- album_api/lib/main.dart
+++ album_api/lib/main.dart
@@ -1,3 +1,6 @@
+
+import 'package:album_api/album_model.dart';
+import 'package:album_api/album_services.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +13,40 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    Future<List<Album>>? _futureAlbums;
+    AlbumServices _apiService = AlbumServices();
 
+   
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("AlbumAPi"),
+        centerTitle: true,
+      
+      ),
+      body: FutureBuilder(
+        future: _futureAlbums, 
+      builder: (context,snapshot){
+        if(snapshot.connectionState == ConnectionState.none){
+          return Center(
+            child: ElevatedButton(onPressed: (){
+          
+            }, child: Text("loading....")
+            )
+          );
+        }else if(snapshot.connectionState == ConnectionState.active || snapshot.connectionState==ConnectionState.waiting){
+          return Center (child: CircleAvatar(),
+          );
+        }
+      else {
+        return Center(child: Text("Somthing went wronng"),);
+      }
+     
+      }
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: (){},
+      child: Icon(Icons.add),
       ),
     );
   }
